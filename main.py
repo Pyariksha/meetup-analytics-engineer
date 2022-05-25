d56942d6b1abbae3d0b2079f8c851a0d655a380a
Pyariksha tiluk <pyar.tiluk@gmail.com>
2022-05-25 21:13:51 +0200

    try

--- main.py
+++ main.py
@@ -19,47 +19,47 @@ credentials = service_account.Credentials.from_service_account_file(
 '''
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'sigma-scheduler-348710-0e55acb5c90d.json'
 '''
-
+'''
 class Preprocess:
-    def __init__(self, bucket_name_read, bucket_name_write):
-        self.bucket_name_read = bucket_name_read
-        self.bucket_name_write = bucket_name_write
-       
-    def get_raw(self, raw_path_gcs):
-        client = Client()
-        bucket_name_read = self.bucket_name_read
-        bucket = client.get_bucket(bucket_name_read)
-        blob = bucket.get_blob(raw_path_gcs)
-        downloaded_json_file = json.loads(blob.download_as_text(encoding="utf-8"))
-        return downloaded_json_file
+        def __init__(self, bucket_name_read, bucket_name_write):
+            self.bucket_name_read = bucket_name_read
+            self.bucket_name_write = bucket_name_write'''
 
-    def transform_raw(self,downloaded_json_file):
-        norm_file = pd.json_normalize(downloaded_json_file, record_path=['rsvps'], record_prefix = 'rsvps_', meta=['name', 'status', 'time', 'duration', 'group_id', 'created', 'description'])
-        norm_file.to_json('events_norm.json', orient='records', lines=True)
-        return norm_file
-    
-    def write_clean_to_gcs(self,norm_file,csv_name):
-        bucket_name_write = self.bucket_name_write
-        norm_file.to_csv('gs://{}/{}'.format(bucket_name_write,csv_name), sep=',')
-        print('Dataframe "norm_file" written as CSV to gcs bucket: {}'.format(bucket_name_write))
+def get_raw(raw_path_gcs, bucket_name_read):#self
+    client = Client()
+    bucket_name_read = bucket_name_read #self
+    bucket = client.get_bucket(bucket_name_read)
+    blob = bucket.get_blob(raw_path_gcs)
+    downloaded_json_file = json.loads(blob.download_as_text(encoding="utf-8"))
+    return downloaded_json_file
 
-    def load_to_bq(self, norm_file, schema):
-        norm_file=norm_file.applymap(str)
-        project_name = 'sigma-scheduler-348710'#gcp project name
-        dataset_name = 'meetup'
-        table_name = 'events'
-        table_id = '{}.{}'.format(dataset_name, table_name)
-        #pandas_gbq.to_gbq(y,table_id, project_id=project_name, if_exists='append')#append tweets to table
-        client = bigquery.Client()
-        job_config = bigquery.LoadJobConfig(
-            #autodetect=True,
-            schema=schema
-            ,write_disposition='WRITE_TRUNCATE',)
-        job = client.load_table_from_dataframe(
-            norm_file, table_id, job_config=job_config
-        )
-        # Wait for the load job to complete.
-        job.result()
+def transform_raw(downloaded_json_file):#self
+    norm_file = pd.json_normalize(downloaded_json_file, record_path=['rsvps'], record_prefix = 'rsvps_', meta=['name', 'status', 'time', 'duration', 'group_id', 'created', 'description'])
+    norm_file.to_json('events_norm.json', orient='records', lines=True)
+    return norm_file
+
+def write_clean_to_gcs(norm_file,csv_name, bucket_name_write):#self
+    bucket_name_write = bucket_name_write #self
+    norm_file.to_csv('gs://{}/{}'.format(bucket_name_write,csv_name), sep=',')
+    print('Dataframe "norm_file" written as CSV to gcs bucket: {}'.format(bucket_name_write))
+
+def load_to_bq(norm_file, schema):
+    norm_file=norm_file.applymap(str)
+    project_name = 'sigma-scheduler-348710'#gcp project name
+    dataset_name = 'meetup'
+    table_name = 'events'
+    table_id = '{}.{}'.format(dataset_name, table_name)
+    #pandas_gbq.to_gbq(y,table_id, project_id=project_name, if_exists='append')#append tweets to table
+    client = bigquery.Client()
+    job_config = bigquery.LoadJobConfig(
+        #autodetect=True,
+        schema=schema
+        ,write_disposition='WRITE_TRUNCATE',)
+    job = client.load_table_from_dataframe(
+        norm_file, table_id, job_config=job_config
+    )
+    # Wait for the load job to complete.
+    job.result()
 
 schema_events = [
             bigquery.SchemaField("rsvps_guests", "STRING"),
@@ -112,11 +112,13 @@ schema_venues = [
             bigquery.SchemaField("description", "STRING"),]
 
 if __name__ == '__main__':
-    runClass = Preprocess('pya_bucket1', 'pya_bucket1')
-    x = runClass.get_raw('raw/events.json')
-    y = runClass.transform_raw(x)
-    runClass.write_clean_to_gcs(y,'events_norm.csv')
-    runClass.load_to_bq(y, schema_events)
+    #runClass = Preprocess('pya_bucket1', 'pya_bucket1')
+    bucket_name_read = 'pya_bucket1'
+    bucket_name_write = 'pya_bucket1'
+    x = get_raw('raw/events.json', bucket_name_read)#runClass.
+    y = transform_raw(x)#runClass.
+    write_clean_to_gcs(y,'events_norm.csv', bucket_name_write)#runClass.
+    load_to_bq(y, schema_events)#runClass.
 
 '''
 def classname(obj):
